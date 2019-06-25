0c51595b30bf80e625c422a84e652ce3a151a799
Stian Hanger <pdnagilum@gmail.com>
2019-06-25 14:59:35 +0200

    Updated for Google Functions Request Wrapper

--- api/gcloud-function/index.js
+++ api/gcloud-function/index.js
@@ -19,135 +19,132 @@ exports.boingfwip = (req, res) => {
         },
         headers: req.headers,
         query: req.query,
-        body: {
-            raw: null,
-            form: null,
-            json: null
-        }
+        body: null
     };
 
-    if (req.rawBody) {
-        obj.body.raw = req.rawBody;
+    try {
+        // x-forwarded-for
+        if (req.headers['x-forwarded-for']) {
+            obj.connection.xForwardedFor = req.headers['x-forwarded-for'];
+        }
 
-        let ct = req.headers['content-type'],
-            boundary;
-
-        if (ct) {
-            if (ct.indexOf(';') > -1) {
-                let values = ct.split(';')
-
-                if (values.length > 0) {
-                    ct = values[0];
-                }
-
-                // Check for boundary.
-                if (values.length > 1) {
-                    values.forEach((value) => {
-                        value = value.trim();
-
-                        if (value.startsWith('boundary=')) {
-                            boundary = value.substr(value.indexOf('=') + 1).trim();
-                        }
-                    });
-                }
-            }
-
-            switch (ct) {
-                case 'multipart/form-data':
-                    if (boundary) {
-                        obj.body.form = {};
-
-                        let entries = req.rawBody.split(boundary);
-
-                        if (entries.length > 0) {
-                            entries.forEach((entry) => {
-                                if (entry === '--') {
-                                    return;
-                                }
-
-                                if (entry.startsWith('\r\n')) {
-                                    entry = entry.substr(2);
-                                }
-
-                                if (entry.endsWith('\r\n')) {
-                                    entry = entry.substr(0, entry.length -2);
-                                }
-
-                                if (entry.indexOf('\r\n\r\n') === -1) {
-                                    return;
-                                }
-
-                                let key = entry.substr(0, entry.indexOf('\r\n\r\n')),
-                                    value = entry.substr(entry.indexOf('\r\n\r\n') +4),
-                                    keys = key.split(';');
-
-                                key = null;
-
-                                if (keys.length > 0) {
-                                    keys.forEach((k) => {
-                                        k = k.trim();
-
-                                        if (k.startsWith('name=')) {
-                                            key = k.substr(5);
-
-                                            if (key.startsWith('"')) {
-                                                key = key.substr(1);
-                                            }
-
-                                            if (key.endsWith('"')) {
-                                                key = key.substr(0, key.length -1);
-                                            }
-                                        }
-                                    });
-                                }
-
-                                if (value.endsWith('\r\n--')) {
-                                    value = value.substr(0, value.length -4);
-                                }
-
-                                obj.body.form[key] = value;
-                            });
-                        }
+        // Body.
+        if (req.rawBody) {
+            obj.body = {
+                raw: req.rawBody,
+                stringified: req.rawBody.toString()
+            };
+    
+            let ct = req.headers['content-type'],
+                boundary;
+    
+            if (ct) {
+                if (ct.indexOf(';') > -1) {
+                    let values = ct.split(';')
+    
+                    if (values.length > 0) {
+                        ct = values[0];
                     }
-
-                    break;
-
-                case 'application/x-www-form-urlencoded':
-                    obj.body.form = {};
-
-                    let entries = req.rawBody.split('&');
-
-                    if (entries.length > 0) {
-                        entries.forEach((entry) => {
-                            let iof = entry.indexOf('=');
-
-                            if (iof === -1) {
-                                return;
+    
+                    // Check for boundary.
+                    if (values.length > 1) {
+                        values.forEach((value) => {
+                            value = value.trim();
+    
+                            if (value.startsWith('boundary=')) {
+                                boundary = value.substr(value.indexOf('=') + 1).trim();
                             }
-
-                            let key = entry.substr(0, iof),
-                                value = entry.substr(iof + 1);
-
-                            obj.body.form[key] = value;
                         });
                     }
-
-                    break;
-
-                case 'application/json':
-                case 'application/javascript':
-                    try {
-                        let json = JSON.parse(req.rawBody);
-
-                        if (json) {
-                            obj.body.json = json;
+                }
+    
+                switch (ct) {
+                    case 'multipart/form-data':
+                        if (boundary) {
+                            obj.body.form = {};
+    
+                            let entries = obj.body.stringified.split(boundary);
+    
+                            if (entries.length > 0) {
+                                entries.forEach((entry) => {
+                                    if (entry === '--') {
+                                        return;
+                                    }
+    
+                                    if (entry.startsWith('\r\n')) {
+                                        entry = entry.substr(2);
+                                    }
+    
+                                    if (entry.endsWith('\r\n')) {
+                                        entry = entry.substr(0, entry.length -2);
+                                    }
+    
+                                    if (entry.indexOf('\r\n\r\n') === -1) {
+                                        return;
+                                    }
+    
+                                    let key = entry.substr(0, entry.indexOf('\r\n\r\n')),
+                                        value = entry.substr(entry.indexOf('\r\n\r\n') +4),
+                                        keys = key.split(';');
+    
+                                    key = null;
+    
+                                    if (keys.length > 0) {
+                                        keys.forEach((k) => {
+                                            k = k.trim();
+    
+                                            if (k.startsWith('name=')) {
+                                                key = k.substr(5);
+    
+                                                if (key.startsWith('"')) {
+                                                    key = key.substr(1);
+                                                }
+    
+                                                if (key.endsWith('"')) {
+                                                    key = key.substr(0, key.length -1);
+                                                }
+                                            }
+                                        });
+                                    }
+    
+                                    if (value.endsWith('\r\n--')) {
+                                        value = value.substr(0, value.length -4);
+                                    }
+    
+                                    obj.body.form[key] = value;
+                                });
+                            }
                         }
-                    }
-                    catch {}
+    
+                        break;
+    
+                    case 'application/x-www-form-urlencoded':
+                        obj.body.form = {};
+    
+                        let entries = obj.body.stringified.split('&');
+    
+                        if (entries.length > 0) {
+                            entries.forEach((entry) => {
+                                let entrySplit = entry.split('=');
 
-                    break;
+                                if (entrySplit.length === 2) {
+                                    obj.body.form[entrySplit[0]] = entrySplit[1];
+                                }
+                            });
+                        }
+    
+                        break;
+    
+                    case 'application/json':
+                    case 'application/javascript':
+                        obj.body.json = JSON.parse(obj.body.stringified);
+                        break;
+                }
             }
         }
+    }
+    catch (ex) {
+        obj.body.ex = ex;
     }
 
     res.json(obj);
